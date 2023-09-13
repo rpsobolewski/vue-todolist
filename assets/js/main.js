0ae9287c7b7c79768ce89cdf50a8e90b346a5708
rpsobolewski <rpsobolewski@gmail.com>
2023-09-14 01:11:04 +0200

    added functions
    add with enter and line-through on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,30 +8,30 @@ createApp({
         done: false,
       },
 
-      tasks: [
-        {
-          text: "ciao",
-          done: true,
-        },
-        {
-          text: "ciao",
-          done: true,
-        },
-      ],
+      tasks: [],
     };
   },
   methods: {
     addTask() {
-      if (this.newTask.length == 0) {
+      if (this.newTask.text.length === 0) {
         alert("scrivi qualcosa");
       } else {
-        this.tasks.push(this.newTask);
-        console.log(this.newTask);
+        this.tasks.push({ text: this.newTask.text });
+        this.newTask.text = ""; // ripulisco il campo input se no tutti i task sono lo stesso task
       }
     },
+
     removeTask(index) {
       this.tasks.splice(index, 1);
     },
-    done(index) {},
+    done(index) {
+      this.tasks[index].done = !this.tasks[index].done; //toggle di done
+    },
+    addTaskOnEnter(event) {
+      if (event.keyCode === 13) {
+        this.addTask();
+        //controllo se è stato premuto invio
+      }
+    },
   },
 }).mount("#app");
